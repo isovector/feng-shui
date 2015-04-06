5a96ed675c93b3496d223067099e1953c1fddc4f
Sandy Maguire <sandy@sandymaguire.me>
2015-04-05 23:22:23 -0400

    principal components

--- src/FengShui.hs
+++ src/FengShui.hs
@@ -1,18 +1,17 @@
-module FengShui (CSV(CSV), toCSV, fromCSV) where
+module FengShui (CSV(CSV), parseCSV, toCSV, principalComponents) where
 
-import Data.List (intercalate)
-import Data.Matrix
+import Control.Monad (liftM2)
+import Data.Complex (Complex( (:+) ))
+import Data.List (intercalate, transpose)
+import Data.List.Split (splitOn)
+import Data.Packed.Matrix
 import Data.Maybe (catMaybes)
+import Numeric.Container
+import Numeric.Extra (floatToDouble)
+import Numeric.LinearAlgebra.Algorithms
 
 
 data CSV a = CSV [String] [[a]] deriving Show
-
-wordsWhen :: (Char -> Bool) -> String -> [String]
-wordsWhen p s =  case dropWhile p s of
-                   "" -> []
-                   s' -> w : wordsWhen p s''
-                     where (w, s'') = break p s'
-
 
 readCell :: String -> Maybe Float
 readCell s = case s of
@@ -22,24 +21,45 @@ readCell s = case s of
 
 parseCSV :: String -> CSV (Maybe Float)
 parseCSV s = CSV labels parsed
-  where rows = map (wordsWhen (==',')) $ lines s
+  where rows = map (splitOn ",") $ lines s
         labels = head rows
         parsed = map (map readCell) $ tail rows
+
+average :: [Float] -> Float
+average = liftM2 (/) sum (fromIntegral . length)
 
 normalizeCSV :: CSV (Maybe Float) -> CSV Float
 normalizeCSV (CSV labels rows) = CSV labels rows'
   where avg i = let defined = catMaybes $ map (!! i) rows
-                 in sum defined / (fromIntegral $ length defined)
+                 in average defined
         rows' = map byRow rows
         byRow r = map replacement $ zip [0..] r
         replacement (i, x) = case x of
                                Just x' -> x'
                                Nothing -> avg i
 
+meanColumns :: [[Float]] -> [Float]
+meanColumns = map average . transpose
+
+meanCSV :: CSV Float -> CSV Float
+meanCSV (CSV l rows) = CSV l rows'
+  where rows' = map (zipWith subtract means) rows
+        means = meanColumns rows
+
 toCSV :: String -> CSV Float
 toCSV = normalizeCSV . parseCSV
 
-fromCSV :: CSV a -> Matrix a
-fromCSV (CSV labels rows) = matrix (length rows) (length labels) go
-  where go (y, x) = (rows !! (y - 1)) !! (x - 1)
+fromCSV :: Element a => CSV a -> Matrix a
+fromCSV (CSV _ rows) = fromLists rows
+
+nthColumn :: Element a => Matrix a -> Int -> [a]
+nthColumn m n = toList . flatten . takeColumns 1 $ dropColumns n m
+
+principalComponents :: CSV Float -> [[Double]]
+principalComponents csv@(CSV labels _) = map (map realPart . nthColumn eigs) [0..lastCol]
+  where m = fromCSV csv
+        cov = m `multiply` trans m
+        complex = cmap ((:+ 0) . floatToDouble) cov
+        lastCol = length labels - 1
+        eigs = snd $ eig complex
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
 import Control.Applicative ((<$>))
+import Control.Monad (forM_)
 import System.Environment (getArgs)
 import FengShui
 
 main = do
     file <- head <$> getArgs
     contents <- readFile file
-    let matlab = fromCSV $ toCSV contents
-    putStrLn $ show matlab
+    let pcs = head . principalComponents $ toCSV contents
+    forM_ pcs (putStrLn . show)
+    return ()
 
