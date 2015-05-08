671a707df4dc2345d0a36736822ac8b4eacf2a47
Sandy Maguire <sandy@sandymaguire.me>
2015-05-08 21:41:56 +0100

    divide by stddev, untransform

--- src/FengShui.hs
+++ src/FengShui.hs
@@ -1,17 +1,37 @@
-module FengShui (CSV(CSV), parseCSV, toCSV, principalComponents) where
+module FengShui ( CSV(CSV)
+                , parseCSV
+                , toCSV
+                , principalComponents
+                , clamp
+                , labels
+                , onlyCols
+                ) where
 
 import Control.Monad (liftM2)
 import Data.Complex (Complex( (:+) ))
-import Data.List (intercalate, transpose)
+import Data.List (intercalate, transpose, elemIndex)
 import Data.List.Split (splitOn)
 import Data.Packed.Matrix
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, isJust)
+import Debug.Trace (trace)
 import Numeric.Container
 import Numeric.Extra (floatToDouble)
 import Numeric.LinearAlgebra.Algorithms
 
 
 data CSV a = CSV [String] [[a]] deriving Show
+
+showTrace :: Show a => a -> a
+showTrace = trace =<< show
+
+labels :: CSV a -> [String]
+labels (CSV l _) = l
+
+onlyCols :: [String] -> CSV a -> CSV a
+onlyCols ls (CSV labels rows) = CSV ls rows'
+  where rows' = map (map snd . filter go . zip [0..]) rows
+        indices = showTrace $ catMaybes $ map (flip elemIndex labels) ls
+        go (a, b) = elem a indices
 
 readCell :: String -> Maybe Float
 readCell s = case s of
@@ -28,6 +48,12 @@ parseCSV s = CSV labels parsed
 average :: [Float] -> Float
 average = liftM2 (/) sum (fromIntegral . length)
 
+stdDev :: [Float] -> Float
+stdDev xs = sqrt . average $ map go xs
+  where go x = square (x - avg)
+        square x = x * x
+        avg = average xs
+
 normalizeCSV :: CSV (Maybe Float) -> CSV Float
 normalizeCSV (CSV labels rows) = CSV labels rows'
   where avg i = let defined = catMaybes $ map (!! i) rows
@@ -38,16 +64,15 @@ normalizeCSV (CSV labels rows) = CSV labels rows'
                                Just x' -> x'
                                Nothing -> avg i
 
-meanColumns :: [[Float]] -> [Float]
-meanColumns = map average . transpose
-
-meanCSV :: CSV Float -> CSV Float
-meanCSV (CSV l rows) = CSV l rows'
-  where rows' = map (zipWith subtract means) rows
-        means = meanColumns rows
+meanAndStdCSV :: CSV Float -> CSV Float
+meanAndStdCSV (CSV l rows) = CSV l rows'
+  where rows' = map (zipWith3 go means devs) rows
+        means = map average $ transpose rows
+        devs = map stdDev $ transpose rows
+        go m d r = (r - m) / d
 
 toCSV :: String -> CSV Float
-toCSV = normalizeCSV . parseCSV
+toCSV = meanAndStdCSV . normalizeCSV . parseCSV
 
 fromCSV :: Element a => CSV a -> Matrix a
 fromCSV (CSV _ rows) = fromLists rows
@@ -56,10 +81,18 @@ nthColumn :: Element a => Matrix a -> Int -> [a]
 nthColumn m n = toList . flatten . takeColumns 1 $ dropColumns n m
 
 principalComponents :: CSV Float -> [[Double]]
-principalComponents csv@(CSV labels _) = map (map realPart . nthColumn eigs) [0..lastCol]
+principalComponents csv@(CSV labels _) = map (map realPart . nthColumn final) [0..lastCol]
   where m = fromCSV csv
         cov = m `multiply` trans m
-        complex = cmap ((:+ 0) . floatToDouble) cov
+        toComplex = cmap ((:+ 0) . floatToDouble)
+        complex = toComplex cov
         lastCol = length labels - 1
         eigs = snd $ eig complex
+        final = multiply (toComplex $ trans m) eigs
+
+clamp :: Double -> [[Double]] -> [[Double]]
+clamp t = fmap (fmap go)
+  where go x = if abs x < t
+                  then 0
+                  else x
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,18 @@ import Control.Applicative ((<$>))
 import Control.Monad (forM_)
 import System.Environment (getArgs)
 import FengShui
+import Debug.Trace (trace)
+
+
+showTrace :: Show a => a -> a
+showTrace = trace =<< show
 
 main = do
     file <- head <$> getArgs
     contents <- readFile file
-    let pcs = head . principalComponents $ toCSV contents
-    forM_ pcs (putStrLn . show)
+    let cols = [ "piano" ]
+        csv = onlyCols cols $ toCSV contents
+        pcs = head . clamp 0 . principalComponents $ csv
+    forM_ (zip pcs $ labels csv) (putStrLn . show)
     return ()
 
